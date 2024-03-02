451960dfbc6a1e7967edb1957648901541b7e74c
KotniSohan98 <kotnisohan09@gmail.com>
2024-03-02 17:37:49 +0530

    removed unwanted variables

--- src/components/ButtonCustomised.js
+++ src/components/ButtonCustomised.js
@@ -1,21 +1,7 @@
 import * as React from "react";
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import { useState } from "react";
-
 export function ButtonCustomised({ name, color, btn_type, handleDelete }) {
-  const [isTrue, setIsTrue] = useState(true);
   return (
-    // <Button
-    //   type={btn_type}
-    //   variant="contained"
-    //   color={color}
-    //   onClick={(e) => handleDelete(e.id)}
-    // >
-    //   {name}
-    // </Button>
-    // <div>{isTrue ? <p>Hell</p> : <p>dd</p>}</div>
-
     <div>
       {name === "DELETE" ? (
         <Button
